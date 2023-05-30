6c1bbeaedf0e8ab8c59afabe3801d8819d26b735
Ellis Hoag <ellis.sparky.hoag@gmail.com>
2023-05-29 23:32:59 -0700

    Inline example json response

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #![no_std]
 #![no_main]
 
+extern crate alloc;
+
+use alloc::alloc::{GlobalAlloc, Layout};
+use alloc::format;
+use chrono::NaiveDateTime;
 use embedded_graphics::{
     mono_font::{ascii::FONT_6X13, MonoTextStyle},
     prelude::*,
@@ -12,17 +17,51 @@ use epd_waveshare::{
 };
 use esp_backtrace as _;
 use esp_hal_smartled::SmartLedsAdapter;
-use esp_println::println;
+// use esp_println::println;
 use hal::{
     clock, gpio,
     peripherals::{Peripherals, SPI2},
     prelude::*,
     pulse_control, spi, timer, Delay, PulseControl, Rtc, IO,
 };
+use serde_json;
 use smart_leds::{
     hsv::{hsv2rgb, Hsv},
     SmartLedsWrite,
 };
+use spin::Mutex;
+
+// Implement a very bad bump allocator so we allocate memory
+struct BadAllocator {
+    next: Mutex<usize>,
+}
+impl BadAllocator {
+    const fn new() -> Self {
+        BadAllocator {
+            next: Mutex::new(0),
+        }
+    }
+}
+#[global_allocator]
+static ALLOCATOR: BadAllocator = BadAllocator::new();
+
+const BUFFER_SIZE: usize = 1024 * 64;
+static mut BUFFER: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
+unsafe impl GlobalAlloc for BadAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let mut next = self.next.lock();
+        let start = *next;
+        assert!(
+            start + layout.size() < BUFFER.len(),
+            "Overflow at {} bytes",
+            start + layout.size()
+        );
+        *next += layout.size();
+        BUFFER.as_mut_ptr().add(start)
+    }
+    // No we don't ever release memory :)
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
+}
 
 type Spi<'a> = spi::Spi<'a, SPI2, spi::FullDuplexMode>;
 type CSPin = gpio::GpioPin<
@@ -144,19 +183,179 @@ fn init<'a>() -> Context<'a> {
     }
 }
 
+fn get_minutes_until_next_arrival() -> i64 {
+    // https://api.511.org/transit/StopMonitoring?api_key=<API_KEY>&agency=SF&format=json&stopcode=13911
+    // https://www.sfmta.com/stops/carl-st-cole-st-13911
+    // Inline an example response until I get WiFi working
+    let raw_str = r###"
+{
+  "ServiceDelivery": {
+    "ResponseTimestamp": "2023-05-28T16:46:32Z",
+    "ProducerRef": "SF",
+    "Status": true,
+    "StopMonitoringDelivery": {
+      "version": "1.4",
+      "ResponseTimestamp": "2023-05-28T16:46:32Z",
+      "Status": true,
+      "MonitoredStopVisit": [
+        {
+          "RecordedAtTime": "2023-05-28T16:46:26Z",
+          "MonitoringRef": "13911",
+          "MonitoredVehicleJourney": {
+            "LineRef": "N",
+            "DirectionRef": "IB",
+            "FramedVehicleJourneyRef": {
+              "DataFrameRef": "2023-05-28",
+              "DatedVehicleJourneyRef": "11300630"
+            },
+            "PublishedLineName": "JUDAH",
+            "OperatorRef": "SF",
+            "OriginRef": "15223",
+            "OriginName": "Judah/La Playa/Ocean Beach",
+            "DestinationRef": "15239",
+            "DestinationName": "Caltrain/Ballpark",
+            "Monitored": true,
+            "InCongestion": null,
+            "VehicleLocation": {
+              "Longitude": "-122.466629",
+              "Latitude": "37.762146"
+            },
+            "Bearing": "75.0000000000",
+            "Occupancy": "seatsAvailable",
+            "VehicleRef": "2014",
+            "MonitoredCall": {
+              "StopPointRef": "13911",
+              "StopPointName": "Carl St & Cole St",
+              "VehicleLocationAtStop": "",
+              "VehicleAtStop": "false",
+              "DestinationDisplay": "Caltrain/Ballpark",
+              "AimedArrivalTime": "2023-05-28T16:52:06Z",
+              "ExpectedArrivalTime": "2023-05-28T16:54:32Z",
+              "AimedDepartureTime": "2023-05-28T16:52:06Z",
+              "ExpectedDepartureTime": null,
+              "Distances": ""
+            }
+          }
+        },
+        {
+          "RecordedAtTime": "2023-05-28T16:46:26Z",
+          "MonitoringRef": "13911",
+          "MonitoredVehicleJourney": {
+            "LineRef": "N",
+            "DirectionRef": "IB",
+            "FramedVehicleJourneyRef": {
+              "DataFrameRef": "2023-05-28",
+              "DatedVehicleJourneyRef": "11300631"
+            },
+            "PublishedLineName": "JUDAH",
+            "OperatorRef": "SF",
+            "OriginRef": "15223",
+            "OriginName": "Judah/La Playa/Ocean Beach",
+            "DestinationRef": "15239",
+            "DestinationName": "Caltrain/Ballpark",
+            "Monitored": true,
+            "InCongestion": null,
+            "VehicleLocation": {
+              "Longitude": "-122.496506",
+              "Latitude": "37.7608376"
+            },
+            "Bearing": "75.0000000000",
+            "Occupancy": "seatsAvailable",
+            "VehicleRef": "2043",
+            "MonitoredCall": {
+              "StopPointRef": "13911",
+              "StopPointName": "Carl St & Cole St",
+              "VehicleLocationAtStop": "",
+              "VehicleAtStop": "false",
+              "DestinationDisplay": "Caltrain/Ballpark",
+              "AimedArrivalTime": "2023-05-28T17:06:06Z",
+              "ExpectedArrivalTime": "2023-05-28T17:24:16Z",
+              "AimedDepartureTime": "2023-05-28T17:06:06Z",
+              "ExpectedDepartureTime": null,
+              "Distances": ""
+            }
+          }
+        },
+        {
+          "RecordedAtTime": "1970-01-01T00:00:00Z",
+          "MonitoringRef": "13911",
+          "MonitoredVehicleJourney": {
+            "LineRef": "N",
+            "DirectionRef": "IB",
+            "FramedVehicleJourneyRef": {
+              "DataFrameRef": "2023-05-28",
+              "DatedVehicleJourneyRef": "11300633"
+            },
+            "PublishedLineName": "JUDAH",
+            "OperatorRef": "SF",
+            "OriginRef": "15223",
+            "OriginName": "Judah/La Playa/Ocean Beach",
+            "DestinationRef": "15239",
+            "DestinationName": "Caltrain/Ballpark",
+            "Monitored": true,
+            "InCongestion": null,
+            "VehicleLocation": {
+              "Longitude": "",
+              "Latitude": ""
+            },
+            "Bearing": null,
+            "Occupancy": null,
+            "VehicleRef": null,
+            "MonitoredCall": {
+              "StopPointRef": "13911",
+              "StopPointName": "Carl St & Cole St",
+              "VehicleLocationAtStop": "",
+              "VehicleAtStop": "",
+              "DestinationDisplay": "Caltrain/Ballpark",
+              "AimedArrivalTime": "2023-05-28T17:29:20Z",
+              "ExpectedArrivalTime": "2023-05-28T17:40:24Z",
+              "AimedDepartureTime": "2023-05-28T17:29:20Z",
+              "ExpectedDepartureTime": null,
+              "Distances": ""
+            }
+          }
+        }
+      ]
+    }
+  }
+}"###;
+    let v = serde_json::from_str::<serde_json::Value>(raw_str).unwrap();
+    let expected_arrival_time = v["ServiceDelivery"]["StopMonitoringDelivery"]
+        ["MonitoredStopVisit"][0]["MonitoredVehicleJourney"]["MonitoredCall"]
+        ["ExpectedArrivalTime"]
+        .as_str()
+        .unwrap();
+
+    // TODO: Need to lookup and track walltime
+    let now = NaiveDateTime::parse_from_str("2023-05-28T16:30:00Z", "%+").unwrap();
+
+    NaiveDateTime::parse_from_str(expected_arrival_time, "%+")
+        .unwrap()
+        .signed_duration_since(now)
+        .num_minutes()
+}
+
 #[entry]
 fn main() -> ! {
-    println!("Initializing...");
+    let next_arrival = get_minutes_until_next_arrival();
+
     let mut ctx = init();
-    println!("done!");
 
     ctx.display.clear(TriColor::White).unwrap();
 
     let style = MonoTextStyle::new(&FONT_6X13, TriColor::Chromatic);
 
-    Text::new("Hello world", Point::new(0, 30), style)
+    Text::new("N Eastbound", Point::new(5, 30), style)
         .draw(&mut ctx.display)
         .unwrap();
+
+    Text::new(
+        format!("{} mins", next_arrival).as_str(),
+        Point::new(5, 50),
+        style,
+    )
+    .draw(&mut ctx.display)
+    .unwrap();
 
     // TODO: Improve the speed of updating frames
     ctx.epd
